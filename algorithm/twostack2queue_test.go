1a3e088cf02d6187124d185c9770960edc4f7a6d
“jony-byqc” <2358681714@qq.com>
2022-07-28 09:28:16 +0800

    [IMP](twostack2queue) 新增两个栈实现队列练习

---
+++ algorithm/twostack2queue_test.go
@@ -0,0 +1,90 @@
+package twostack2queue_test
+
+import (
+	"fmt"
+	"testing"
+)
+
+// Queue 使用两个栈串联，可以实现先进先出。但是，得注意以下两点:
+//
+//队列在入列时，stack2必须为空，stack1满员，保证顺序。
+//队列在出列时，stack1必须为空，stack2满员
+// 用两个stack，作一个队列
+type Queue struct {
+	stack1 *Stack
+	stack2 *Stack
+}
+
+func NewQueue(col int) *Queue {
+	return &Queue{
+		stack1: NewStack(col),
+		stack2: NewStack(col),
+	}
+}
+func (q Queue) Push(elem int) {
+	for q.stack2.Len() > 0 {
+		q.stack1.Push(q.stack2.Pop())
+	}
+	q.stack1.Push(elem)
+}
+
+func (q Queue) Pop() int {
+	for q.stack1.Len() > 0 {
+		q.stack2.Push(q.stack1.Pop())
+	}
+	rs := q.stack2.Pop()
+	return rs
+}
+
+type Stack struct {
+	element []int
+}
+
+func NewStack(col int) *Stack {
+	return &Stack{
+		element: make([]int, 0, col),
+	}
+}
+
+func (s Stack) Len() int {
+	return len(s.element)
+}
+
+func (s *Stack) Push(elem int) {
+	s.element = append(s.element, elem)
+}
+
+func (s *Stack) Pop() int {
+	if len(s.element) == 0 {
+		return 0
+	}
+	tmp := s.element[len(s.element)-1]
+	s.element = s.element[:len(s.element)-1]
+	return tmp
+}
+
+func TestStack(t *testing.T) {
+	stack := NewStack(10)
+
+	stack.Push(5)
+	stack.Push(6)
+
+	fmt.Println(stack.Pop())
+	fmt.Println(stack.Pop())
+}
+
+func TestQueue(t *testing.T) {
+	queue := NewQueue(10)
+
+	queue.Push(5)
+	queue.Push(6)
+	queue.Push(7)
+
+	fmt.Println(queue.Pop())
+	fmt.Println(queue.Pop())
+
+	queue.Push(8)
+
+	fmt.Println(queue.Pop())
+	fmt.Println(queue.Pop())
+}
--- filedir/main.go
+++ filedir/main.go
@@ -2,12 +2,16 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
+	"log"
 	"os"
+	"path/filepath"
 )
 
 func main() {
 	//fileObej, err := os.Open("./log/a.log") // 读取文件，可以使用相对路径
 	ReadFile("./log/a.log")
+	main1()
 }
 func ReadFile(filename string) {
 	file, _ := os.OpenFile(filename, os.O_RDONLY, 0400)
@@ -22,4 +26,22 @@ func ReadFile(filename string) {
 		length += int64(n)
 	}
 	fmt.Println(string(content_buffer))
+}
+func main1() {
+	//pwd,_ := os.Getwd() //获取当前目录
+	//获取文件或目录相关信息
+	fileInfoList, err := ioutil.ReadDir("./log")
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(len(fileInfoList))
+	for i := range fileInfoList {
+		fmt.Println(fileInfoList[i].ModTime()) //打印当前文件或目录下的文件或目录名
+
+	}
+	filepath.Walk("./log", func(path string, info os.FileInfo, err error) error {
+		fmt.Println(path)        //打印path信息
+		fmt.Println(info.Name()) //打印文件或目录名
+		return nil
+	})
 }
