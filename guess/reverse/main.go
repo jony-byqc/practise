f4aba4bd5ff573912df774fa4b6f9b31d320672c
“jony-byqc” <2358681714@11.com>
2023-05-14 13:21:38 +0800

    feat(slice): slice特性练习

---
+++ guess/reverse/main.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	src := "你好abc啊哈哈"
+	dst := reverse([]rune(src))
+	fmt.Printf("%v\n", string(dst))
+}
+
+func reverse(s []rune) []rune {
+	for i, j := 0, len(s)-1; i < j; i, j = i+1, j-1 {
+		s[i], s[j] = s[j], s[i]
+	}
+	return s
+}
